42967b4f006fd9cdd318310bb88bcabb01353672
Giliard Godoi <giliard.godoi@gmail.com>
2023-02-06 22:53:37 -0300

    add function to write a stp file from a tree nx.Graph

--- xsteiner/io.py
+++ xsteiner/io.py
@@ -13,7 +13,7 @@ def read(path_file: str):
     # with open(path_file, 'r') as file:
     #    text = file.read()
     with path_file.open() as file:
-        text = file.read_text()
+        text = file.read()
 
     ptr_edges = re.compile(r"E (\d+) (\d+) (\d+)", re.M)
     ptr_terminals = re.compile(r"T (\d+)", re.M)
@@ -23,7 +23,7 @@ def read(path_file: str):
     ptr_n_terminals = re.compile(r"Terminals (\d+)", re.M)
 
     G = nx.Graph()
-    G.graph["terminals"] = list()
+    terminals = list()
 
     n_count_edges = 0
     n_count_terminals = 0
@@ -43,8 +43,10 @@ def read(path_file: str):
             break
         else:
             t = int(m.group(1))
-            G.graph["terminals"].append(t)
+            terminals.append(t)
             n_count_terminals += 1  # optional validation
+
+    G.graph["terminals"] = frozenset(terminals)
 
     n_nodes = ptr_n_nodes.search(text)
     n_edges = ptr_n_edges.search(text)
@@ -61,3 +63,44 @@ def read(path_file: str):
     assert n_count_terminals == n_terminals
 
     return G
+
+def write_stp(filename, 
+                T: nx.Graph, 
+                terminals,
+                name= 'default name',
+                creator='xsteiner',
+                remark='Created with xsteiner lib'
+            ):
+    
+    nro_terminals = len(terminals)
+    tx_edges = '\n'.join([f"E {v} {u} {T[v][u].get('weight', 0)}" for v, u in T.edges])
+    tx_terminals = '\n'.join([f"T {t}" for t in terminals])
+
+    template = f'''xxxxxxxx STP File, STP Format Version 1.0
+
+SECTION Comment
+Name    "{name}"
+Creator "{creator}"
+Remark  "{remark}"
+END
+
+SECTION Graph
+Nodes {T.number_of_nodes()}
+Edges {T.number_of_edges()}
+{tx_edges}
+END
+
+SECTION Terminals
+Terminals {nro_terminals}
+{tx_terminals}
+END
+
+EOF
+
+'''
+
+    with open(filename, 'w') as file:
+        file.write(template)
+    
+    return True
+    
