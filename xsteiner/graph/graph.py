439b0b853cc52461cf9caef43973f47637d36a52
Giliard Godoi <giliard.godoi@gmail.com>
2025-02-07 23:40:08 -0300

    graph representation

---
+++ xsteiner/graph/graph.py
@@ -0,0 +1,139 @@
+from typing import Iterable
+from .edge import Edge
+
+class Graph:
+
+    def __init__(self):
+        self.data = dict()
+
+    def add_edge(self, x0, x1, weight=None):
+        edge = Edge(x0, x1, weight=weight)
+        if x0 not in self.data:
+            self.add_node(x0)
+        if x1 not in self.data:
+            self.add_node(x1)
+        self.data[x0].add(edge)
+        self.data[x1].add(edge)
+        return True
+
+    def add_node(self, node):
+        if node == None:
+            raise ValueError('Cannot assign None as Graph Node')
+        if node in self.data:
+            raise ValueError(f'Node already exists: {node}')
+        self.data[node] = set()
+        return True
+
+    def adjacents(self, node):
+        if self.has_node(node):
+            raise ValueError(f'Node not in graph: {node}')
+        for edge in self.data[node]:
+            other = edge.x if node == edge.y else edge.y
+            yield other
+
+    def adjacents_edges(self, node):
+        if not self.has_node(node):
+            raise ValueError(f'Node not in graph: {node}')
+        yield from self.data.get(node)
+
+    def edges(self):
+        visited = set()
+        for node in self.data.keys():
+            for edge in self.data[node]:
+                if edge in visited:
+                    continue
+                visited.add(edge)
+                yield edge
+
+    def nodes(self):
+        yield from self.data.keys()
+
+    def degree(self, node):
+        if self.has_node(node):
+            raise ValueError(f'Node not in graph: {node}')
+        return len(self.data[node])
+
+    def has_edge(self, *args):
+
+        if len(args) == 1 and isinstance(args[0], Edge):
+            edge = args[0]
+        elif len(args) == 1:
+            raise TypeError(f'Args is not a Edge type: received {type(args[0])}')
+        elif len(args) == 2:
+            edge = Edge(args[0], args[1], weight=None)
+        elif len(args) == 3:
+            edge = Edge(args[0], args[1], weight=args[2])
+        else:
+            raise ValueError(f'Parameters received: {args=}')
+
+        return (self.has_node(edge.x)) \
+                and (self.has_node(edge.y)) \
+                and (edge in self.data.get(edge.x)) \
+                and (edge in self.data.get(edge.y))
+
+    def has_node(self, node):
+        return (node in self.data)
+
+    def remove_node(self, node):
+        if node not in self.data:
+            raise ValueError(f'Node not found in graph: {node}')
+
+        if not self.data[node]:
+            return
+
+        for edge in self.data[node]:
+            u, v = edge
+            other = v if node == u else u
+            if other in self.data and self.data[other]:
+                self.data[other].remove(edge)
+
+        del self.data[node]
+
+    def remove_edge(self, *args):
+        if len(args) == 1 and isinstance(args[0], Edge):
+            edge = args[0]
+        elif len(args) == 1:
+            raise TypeError(f'Args is not a Edge type: received {type(args[0])}')
+        elif len(args) == 2:
+            edge = Edge(args[0], args[1], weight=None)
+        elif len(args) == 3:
+            edge = Edge(args[0], args[1], weight=args[2])
+        else:
+            raise ValueError(f'Parameters received: {args!s}')
+
+        if not self.has_edge(edge):
+            raise ValueError(f'Edge not found in graph: {edge!s}')
+
+        self.data[edge.x].remove(edge)
+        self.data[edge.y].remove(edge)
+
+    def n_nodes(self):
+        return len(self.data)
+
+class SteinerGraphProblemInstance(Graph):
+
+    def __init__(self):
+        super().__init__()
+        self.nro_nodes = 0
+        self.nro_edges = 0
+        self.nro_terminals = 0
+        self.name = None
+        self.remark = None
+        self.creator = None
+        self.file_name = None
+        self.__terminals = None
+
+    @property
+    def terminals(self):
+        return self.__terminals
+
+    @terminals.setter
+    def terminals(self, values):
+        pass
+
+    def update_terminals(self, terminals: Iterable):
+
+        if self.__terminals is not None:
+            RuntimeError('Cannot reasign terminals nodes for an already instatiate steiner graph')
+
+        self.__terminals = set(terminals)
