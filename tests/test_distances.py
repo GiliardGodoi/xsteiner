def225bd9a0b7d0f0f89681f94f6444412dce2b3
Giliard Godoi <giliard.godoi@gmail.com>
2025-02-09 13:15:51 -0300

    update distance shortest path algorithm

---
+++ tests/test_distances.py
@@ -0,0 +1,51 @@
+import pytest
+from collections import defaultdict
+from xsteiner.graph.distances import (
+    shortest_path
+)
+from xsteiner.graph.graph import Graph
+
+@pytest.fixture
+def empty_graph():
+    return Graph()
+
+@pytest.fixture
+def medium_graph():
+    g = Graph()
+    nodes = range(1, 21)
+    for node in nodes:
+        g.add_node(node)
+    edges = [
+        (1, 2, 3), (2, 3, 4), (3, 4, 5), (4, 5, 6), (5, 6, 7),
+        (6, 7, 8), (7, 8, 9), (8, 9, 10), (9, 10, 11), (10, 11, 12),
+        (11, 12, 13), (12, 13, 14), (13, 14, 15), (14, 15, 16), (15, 16, 17),
+        (16, 17, 18), (17, 18, 19), (18, 19, 20), (19, 20, 21), (20, 1, 32)
+    ]
+    for edge in edges:
+        g.add_edge(*edge)
+    return g
+
+def test_shortest_path_return_type_obj(medium_graph):
+    node = 10
+    dist, prev = shortest_path(medium_graph, node)
+    assert isinstance(dist, defaultdict)
+    assert isinstance(prev, dict)
+
+def test_shortest_path_with_empty_graph(empty_graph):
+    with pytest.raises(ValueError):
+        _, _ = shortest_path(empty_graph, 10)
+
+def test_shortest_path_with_inexistent_node_in_graph(medium_graph):
+        with pytest.raises(ValueError):
+            _, _ = shortest_path(medium_graph, 55)
+
+def test_shortest_path_with_medium_graph(medium_graph):
+    dist, prev = shortest_path(medium_graph, 5)
+
+    assert prev[5] is None
+    assert dist[5] == 0
+    assert prev[1] == 2
+    assert dist[1] == 18
+    assert prev[20] == 1
+    assert dist[20] == (18 + 32)
+
---
+++ xsteiner/graph/distances.py
@@ -0,0 +1,54 @@
+from collections import defaultdict
+from xsteiner.pqueue.pqueue import PriorityQueue
+
+def shortest_path(graph, source):
+    ''' Dijkstra Algorithm: It computes the lowest distance between a source vertice and
+    all the other vertices.
+
+    Parameters
+        graph : GraphDictionary
+        source : <a vertice from the graph>
+
+    Returns
+        dist : dict
+            <vertice : distance from source> - distance dictionary from the source to vertice
+        prev : dict
+            < vertice : previous > - a dictionary representing a previous node
+
+    Notes:
+        An early implementation has O(n^2) complexity.
+        Many improvements has been proposed across the years.
+        They are more complicated, though.
+
+        Based in Chirantan Sharma's code
+        <https://github.com/cs-oak/Fibonacci-Heaps/blob/master/dijkstra.py>
+        Its use an other Priority Queue implemantation which is not used by Sharma.
+    '''
+    if not graph.has_node(source):
+        raise ValueError(f'Source node is not in graph: {source}')
+
+    dist = defaultdict(lambda : float("inf"))
+    dist[source] = 0
+
+    prev = {source : None}
+    done = dict()
+
+    pqueue = PriorityQueue()
+    pqueue.push(0, (0, source))
+
+    while len(pqueue):
+        _, (dist_u, u) = pqueue.pop()
+
+        if u in done :
+            continue
+        done[u] = True
+        for edge in graph.adjacents_edges(u):
+            v = edge.y if edge.x == u else edge.x
+            new_dist_to_v = dist_u + edge.weight
+            # esse if só da False quando: o vertice ja estiver em dist (visitado) e a distancia ja for a menor
+            if (not v in dist) or (dist[v] > new_dist_to_v):
+                dist[v] = new_dist_to_v
+                prev[v] = u
+                pqueue.push(new_dist_to_v,(new_dist_to_v, v))
+
+    return dist, prev
