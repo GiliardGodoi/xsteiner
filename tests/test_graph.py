44a656dd535883b20906b39cc8c92ac90a35efdf
Giliard Godoi <giliard.godoi@gmail.com>
2025-02-09 22:12:08 -0300

    update test_graph

--- tests/test_graph.py
+++ tests/test_graph.py
@@ -3,15 +3,34 @@ from xsteiner.graph.graph import Graph
 from xsteiner.graph.edge import Edge
 
 @pytest.fixture
+def list_edges():
+    return [
+            Edge(4, 5, 9),
+            Edge(1, 2, 5),
+            Edge(1, 2, None),
+            Edge(12, 13, 14),
+            Edge(12, 13, 15),
+        ]
+
+@pytest.fixture
 def empty_graph():
     return Graph()
+
+@pytest.fixture
+def star_graph():
+    g = Graph()
+    center = 20
+    weights = [9, 8, 1, 0, 3, 6, 7, 15]
+    for i, w in enumerate(weights, start=1):
+        g.add_edge(i, center, w)
+    return g
 
 @pytest.fixture
 def small_graph():
     g = Graph()
     g.add_node(1)
     g.add_node(2)
-    g.add_edge(1, 2, weight=5)
+    g.add_edge(1, 2, 5)
     return g
 
 @pytest.fixture
@@ -46,26 +65,29 @@ def test_add_node_to_empty_graph(graph, node, expected, request):
     assert len([n for n in graph.nodes()]) == expected
     assert node in [n for n in graph.nodes()]
 
+
 def test_add_none_as_node(empty_graph):
     with pytest.raises(ValueError):
         empty_graph.add_node(None)
+
 
 def test_add_an_already_existing_node(small_graph):
     small_graph.add_node(3)
     with pytest.raises(ValueError):
         small_graph.add_node(2)
 
-@pytest.mark.parametrize('graph,node,expected',[
-    ('small_graph', 2, True),
-    ('small_graph', 5, False),
-    ('small_graph', 'X', False),
-    ('small_graph', None, False)
-])
-def test_has_node(graph, node, expected, request):
-    graph = request.getfixturevalue(graph)
-    assert graph.has_node(node) == expected
 
-def test_add_edge(empty_graph):
+def test_adjacency(medium_graph):
+    cyclic = medium_graph
+    start = 1
+    assert cyclic.has_node(start)
+    adjs = [v for v in cyclic.adjacents(start)]
+    assert len(adjs) == 2
+    assert 20 in adjs
+    assert 2 in adjs
+
+
+def test_add_edge_with_args(empty_graph):
 
     empty_graph.add_edge(1, 2, 80)
     empty_graph.add_edge(2, 3, 0)
@@ -78,6 +100,23 @@ def test_add_edge(empty_graph):
 
     edges = [e for e in empty_graph.edges()]
     assert len(edges) == 5
+
+def test_add_edges_from_list_of_edges(empty_graph, list_edges):
+    for edge in list_edges:
+        empty_graph.add_edge(edge)
+    assert len(list_edges) == len([e for e in empty_graph.edges()])
+
+
+@pytest.mark.parametrize('graph,node,expected',[
+    ('small_graph', 2, True),
+    ('small_graph', 5, False),
+    ('small_graph', 'X', False),
+    ('small_graph', None, False)
+])
+def test_has_node(graph, node, expected, request):
+    graph = request.getfixturevalue(graph)
+    assert graph.has_node(node) == expected
+
 
 @pytest.mark.parametrize('graph_fixture,edge,expected',[
     ('empty_graph',  Edge(4, 5, 9),    False),
@@ -103,17 +142,21 @@ def test_has_edge_with_args(graph_fixture, edge, expected, request):
     graph = request.getfixturevalue(graph_fixture)
     assert graph.has_edge(*edge) == expected
 
+
 def test_has_edge_raising_exception(small_graph):
     with pytest.raises(ValueError):
         small_graph.has_edge(2)
+
 
 @pytest.mark.skip(reason="Test not implemented yet")
 def test_remove_edge():
     assert False, 'Something went wrong!'
 
+
 @pytest.mark.skip(reason="Test not implemented yet")
 def test_graphs_that_share_edges():
     assert False
+
 
 def test_adjacents_edges(medium_graph):
     u = 20
@@ -126,3 +169,10 @@ def test_adjacents_edges(medium_graph):
 
     weights = [edge.weight for edge in edges]
     assert sum(weights) == 53
+
+def test_degree_method(star_graph):
+    leafs = [v for v in star_graph.nodes() if star_graph.degree(v) == 1]
+    assert len(leafs) == 8
+    assert star_graph.degree(20) == 8
+    with pytest.raises(ValueError):
+        star_graph.degree(101)
