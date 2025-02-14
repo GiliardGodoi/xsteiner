6f88dab81154a579608ab84b34037a04e1f6f6e3
Giliard Godoi <giliard.godoi@gmail.com>
2025-02-14 15:21:31 -0300

    add methods

--- xsteiner/graph/operators.py
+++ xsteiner/graph/operators.py
@@ -1,5 +1,10 @@
+import random
 from collections import deque
-from xsteiner.graph.graph import Graph
+from xsteiner.graph.graph import (
+    Graph,
+    SteinerGraphProblemInstance
+)
+from xsteiner.disjointset import DisjointSet
 
 
 def union(red:Graph, blue:Graph):
@@ -19,6 +24,13 @@ def union(red:Graph, blue:Graph):
         if not subgraph.has_edge(edge):
             subgraph.add_edge(edge)
 
+    return subgraph
+
+def intersection(red:Graph, blue:Graph):
+    subgraph = Graph()
+    for edge in red.edges():
+        if blue.has_edge(edge):
+            subgraph.add_edge(edge)
     return subgraph
 
 def induced_subgraph(subset: Graph, base: Graph):
@@ -42,6 +54,45 @@ def induced_subgraph(subset: Graph, base: Graph):
 
     return subgraph
 
+def check_cycle_dfs(graph:Graph, start):
+    visited = set()
+    vertices = set(graph.nodes())
+    while vertices:
+        start = vertices.pop()
+        stack = [(start, None)]
+        while stack:
+            node, parent = stack.pop()
+            if node in visited:
+                continue
+            visited.add(node)
+            for neighbor in graph.adjacents(node):
+                if neighbor == parent:
+                    continue
+                if neighbor in visited:
+                    return True
+                stack.append((neighbor, node))
+        vertices = vertices - visited
+    return False
+
+def check_cycle_union_find(graph:Graph):
+    ds = DisjointSet()
+    for v in graph.vertices: ds.make_set(v)
+    for edge in graph.edges():
+        comp_x = ds.find(edge.x)
+        comp_y = ds.find(edge.y)
+        if comp_x == comp_y:
+            return True
+        ds.union(edge.x, edge.y)
+    return False
+
+def is_connected(graph:Graph):
+    ds = DisjointSet()
+    for v in graph.nodes(): ds.make_set(v)
+    for edge in graph.edges(): ds.union(edge.x, edge.y)
+    components = set(ds.find(v) for v in graph.nodes())
+    return len(components) == 1
+
+
 def is_subset(sub:Graph, super:Graph):
     """
     Check if one graph is a subset of another graph.
@@ -53,6 +104,34 @@ def is_subset(sub:Graph, super:Graph):
     """
     set(sub.nodes()).issubset(super.nodes()) \
         and set(sub.edges()).issubset(super.edges())
+
+def is_steiner_tree(tree:Graph, stpg:SteinerGraphProblemInstance):
+    terminals = stpg.terminals
+
+    def is_terminal(t):
+        return t in terminals
+
+    has_cycle = check_cycle_union_find(tree, start)
+    if has_cycle: return False
+
+    T_edges = list(tree.edges())
+    T_nodes = list(tree.nodes())
+    if len(T_edges) != len(T_nodes) - 1: return False
+
+    all_terminals_in = all(tree.has_node(t) for t in terminals)
+    if not all_terminals_in: return False
+
+    all_leaves_are_terminals = all(is_terminal(t) for t in T_nodes if tree.degree(t) == 1)
+    if not all_leaves_are_terminals: return False
+
+    all_edges_are_reliable = all(stpg.has_edge(edge) for edge in T_edges)
+    if not all_edges_are_reliable: return False
+
+    tree_is_connected = is_connected(tree)
+    if not tree_is_connected: return False
+    start = random.choice(T_nodes)
+
+    return True
 
 def pruning_tree(tree: Graph, terminals : set):
     """
