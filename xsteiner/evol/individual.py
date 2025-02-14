801e36e97940df1afcde9979464fec14ca10e4da
Giliard Godoi <giliard.godoi@gmail.com>
2025-02-14 15:24:08 -0300

    update individuals to correspond to evol.individuals

--- xsteiner/evol/individual.py
+++ xsteiner/evol/individual.py
@@ -1,3 +1,10 @@
+import random
+from typing import (
+    Callable,
+    Any,
+    Optional
+)
+
 class Individual:
     """
     Represents an individual in an evolutionary algorithm.
@@ -19,16 +26,35 @@ class Individual:
 
     _instance_count = 0
 
-    def __init__(self, representation):
-        self._forma = representation
-        self._cost = None
-        self._fitness = None
+    def __init__(self, chromosome: Any, fitness: Optional[float] = None):
+        self._chromosome = chromosome
+        self._cost = fitness
+        self._fitness = fitness
+        self.age = 0
         Individual._instance_count += 1
-        self.seq_id = Individual._instance_count
+        self.id = Individual._instance_count
+
+    def __repr__(self):
+        return f"<individual id:{self.id} fitness:{self.fitness}>"
+
+    @classmethod
+    def from_dict(cls, data: dict) -> 'Individual':
+        """Load an Individual from a dictionary.
+
+        :param data: Dictionary containing the keys 'age', 'chromosome', 'fitness' and 'id'.
+        :return: Individual
+        """
+        result = cls(chromosome=data['chromosome'], fitness=data['fitness'])
+        result.age = data['age']
+        result.id = data['id']
+        return result
 
     @classmethod
     def get_instance_count(cls):
         return cls._instance_count
+
+    def __post_evaluate(self, result: float):
+        self.fitness = result
 
     @property
     def cost(self):
@@ -39,40 +65,51 @@ class Individual:
     @cost.setter
     def cost(self, value):
         self._cost = value
+        self._fitness = value
 
     @property
     def fitness(self):
-        if self._fitness is None:
-            raise ValueError(f'Individual not evaluated yet. Id: {self.seq_id}')
-        return self.__fitness
+        return self._fitness
 
     @fitness.setter
     def fitness(self, value):
         self._fitness = value
 
     @property
-    def forma(self):
-        return self._forma
+    def chromosome(self):
+        return self._chromosome
 
-    @forma.setter
-    def forma(self, value):
-        self._forma = value
+    @chromosome.setter
+    def chromosome(self, value):
+        self._chromosome = value
         self._cost = None
         self._fitness = None
 
     @property
     def tree(self):
-        return self._forma
+        return self._chromosome
 
     @tree.setter
-    def forma(self, value):
-        self._forma = value
+    def chromosome(self, value):
+        self._chromosome = value
         self._cost = None
         self._fitness = None
 
-    def evaluate(self, eval_func, *args, **kargs):
-        self._cost = eval_func(self._forma, *args, **kargs)
+    def evaluate(self, eval_function, *args, lazy=False, **kargs):
+        self._cost = eval_function(self._chromosome, *args, **kargs)
         self._fitness = self._cost
 
     def is_evaluated(self):
         return not (self._cost is None)
+
+    def mutate(self, mutate_function: Callable[..., Any], probability: float = 1.0, **kwargs):
+        """Mutate the chromosome of the individual.
+
+        :param mutate_function: Function that accepts a chromosome and returns a mutated chromosome.
+        :param probability: Probability that the individual mutates.
+            The function is only applied in the given fraction of cases.
+            Defaults to 1.0.
+        :param kwargs: Arguments to pass to the mutation function.
+        """
+        if probability == 1.0 or random.random() < probability:
+            self.chromosome = mutate_function(self.chromosome, **kwargs)
